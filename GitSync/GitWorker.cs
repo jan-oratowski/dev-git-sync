dbec84a019430cdb95c0a699381efdef197e0e2c
Jan Oratowski <jan.oratowski@delivered.io>
2019-10-07 13:28:09 +0200

    proof of concept

---
+++ GitSync/GitWorker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace GitSync
+{
+    public class GitWorker
+    {
+        private readonly List<string> _services;
+
+        public GitWorker(List<string> services)
+        {
+            _services = services;
+        }
+
+        public void Run(string pathToRepos = null)
+        {
+            if (string.IsNullOrEmpty(pathToRepos))
+                pathToRepos = AppContext.BaseDirectory;
+
+            foreach (var directory in System.IO.Directory.GetDirectories(pathToRepos))
+            {
+                PullGitRepo(directory);
+                _services.ForEach(s => PushGitRepo(s, directory));
+            }
+        }
+
+        private void PullGitRepo(string repoPath)
+        {
+            Console.WriteLine($"Pulling {repoPath}");
+            using (var process = new Process())
+            {
+                process.StartInfo.UseShellExecute = true;
+                process.StartInfo.FileName = "git.exe";
+                process.StartInfo.Arguments = "pull origin";
+                process.StartInfo.WorkingDirectory = repoPath;
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+
+        private void PushGitRepo(string service, string repoPath)
+        {
+            Console.WriteLine($"Pushing {repoPath} to {service}");
+            using (var process = new Process())
+            {
+                process.StartInfo.UseShellExecute = true;
+                process.StartInfo.FileName = "git.exe";
+                process.StartInfo.Arguments = $"push {service}";
+                process.StartInfo.WorkingDirectory = repoPath;
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+
+    }
+}
--- GitSync/Program.cs
+++ GitSync/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GitSync
 {
@@ -6,7 +7,10 @@ namespace GitSync
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var repos = new List<string> {"gitlab"};
+            var worker = new GitWorker(repos);
+
+            worker.Run("C:\\test");
         }
     }
 }
