2314138b0178c87e7f99ee896c78d56966c93911
Jan Oratowski <jan.oratowski@delivered.io>
2019-10-07 13:31:09 +0200

    changed to run as a while loop

--- GitSync/Program.cs
+++ GitSync/Program.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace GitSync
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var repos = new List<string> {"gitlab"};
             var worker = new GitWorker(repos);
 
-            worker.Run("C:\\test");
+            var path = string.Empty;
+            if (args.Length > 0)
+                path = args[0];
+
+            while (true)
+            {
+                worker.Run(path);
+                Thread.Sleep(new TimeSpan(6,0,0));
+            }
+            
         }
     }
 }
