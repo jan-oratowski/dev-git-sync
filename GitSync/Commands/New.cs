74375cf45b3f720a9b081e5e880b711401e4cc99
Jan Oratowski <jan.oratowski@gmail.com>
2020-04-30 16:26:11 +0200

    Create New.cs

---
+++ GitSync/Commands/New.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitSync.Commands
+{
+    class New : ICommand
+    {
+        public string Argument => "new";
+        public void DoWork()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
